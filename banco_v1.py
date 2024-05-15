2523dfbe6dd5dfd16bf4d9b048a2568b1e5a0117
Vinicius <vinny.gab18@gmail.com>
2024-05-15 12:47:23 -0300

    Versão finalizada. Adicionado else's para saldo acima da balança e de 500 e uma mudança na lista de operacões

--- banco_v1.py
+++ banco_v1.py
@@ -1,6 +1,5 @@
 # Variáveis
-
-menu = 0
+menu = ""
 
 balanca = 300
 operacoes = []
@@ -14,45 +13,57 @@ LIMITE_SAQUES = 3
 print("Bem vindo ao sistema bancário VGOmes!\n")
 
 # Operações
-while menu != 4:
+while menu != 0:
     menu = int(input("""OPERAÇÃO: 
             1 - Depósito
             2 - Saque
             3 - Extrato
-            4 - Sair
+            0 - Sair
       """))
     
     if menu == 1:
         deposito = float(input("Digite o valor a ser depositado, em R$: "))
         if deposito > 0:
             balanca += deposito
+            operacoes.append(f"Deposito: + {deposito:.2f} R$")
             print(f"Depósito no valor de R$ {deposito:.2f} realizado. \n")
-            operacoes.append(deposito)
+
+        else:
+            print("Digite um valor válido. \n")
 
     elif menu == 2:
         if nsaques < LIMITE_SAQUES:
             saque = float(input("Digite o valor a ser sacado, em R$: "))
-            if saque <= balanca:
+            if saque <= balanca and saque <= 500:
                 balanca -= saque
                 nsaques += 1
+                operacoes.append(f"Saque: - {saque:.2f} R$")
                 print(f"Saque no valor de R$ {saque:.2f} realizado. \n")
-                operacoes.append(saque*(-1))
+            
+            elif saque > balanca:
+                print("Saldo insuficiente para realizar a operação! Tente novamente.\n")
+            
+            elif saque > 500:
+                print("Limite de saque excedido! O limite é 500.00 R$\n")
+
+            else:
+                print("Digite um valor válido. \n")
         else:
             print("Limite de saques diários atingido! Tente novamente amanhã. \n")
 
     elif menu == 3:
         print("Extrato".center(18, "-"))
 
-        print("Ações: ")
-        for operacao in operacoes:
-            if operacao > 0:
-                print(f"{(operacoes.index(operacao))+1}. Depósito = + {operacao:.2f} R$")
-            else:
-                print(f"{(operacoes.index(operacao))+1}. Saque = {operacao:.2f} R$")
+        print("Operações: ")
+        if len(operacoes) == 0:
+            print("Nenhuma operação realizada. \n") 
+        else:
+            for nop, operacao in enumerate(operacoes):
+                print(f"{nop+1}. {operacao}")
 
         print(f"Saldo: {balanca:.2f} R$ \n")
 
-    elif menu == 4:
+    elif menu == 0:
         break
 
     else:
